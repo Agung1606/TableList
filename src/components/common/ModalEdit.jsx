c1e0a82fe24b4d2c0244bd34e5651545b56a62ea
agungsaputra <agngsptra928@gmail.com>
2023-05-21 15:02:44 +0700

    for edit modal is done i hope you like it

--- src/components/common/ModalEdit.jsx
+++ src/components/common/ModalEdit.jsx
@@ -7,6 +7,7 @@ import {
   Toolbar,
   IconButton,
   TextField,
+  useMediaQuery
 } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
 
@@ -23,15 +24,23 @@ const inputStyle = {
 };
 
 function ModalEdit({ item, handleCloseModal }) {
+  const isDesktop = useMediaQuery("(min-width: 1200px)");
   return (
     <Box
       sx={{
-        backgroundColor: "black",
+        background: "linear-gradient(315deg, #2b4162 0%, #12100e 74%)",
         width: "100%",
-        height: "auto",
+        height: "100%",
       }}
     >
-      <AppBar sx={{ position: "relative", background: "none" }}>
+      <AppBar
+        sx={{
+          position: "relative",
+          background: "none",
+          boxShadow: "none",
+          borderBottom: "1px solid #485461",
+        }}
+      >
         <Toolbar>
           <IconButton
             edge="start"
@@ -41,24 +50,42 @@ function ModalEdit({ item, handleCloseModal }) {
           >
             <CloseIcon sx={{ fontSize: 35 }} />
           </IconButton>
-          <Typography sx={{ ml: 5, flex: 1 }} variant="h5" component="div">
+          <Typography sx={{ ml: 4, flex: 1 }} variant="h5" component="div">
             Edit
           </Typography>
         </Toolbar>
       </AppBar>
-      <Box sx={{ display: "flex", marginX: 3 }}>
-        <img alt={`img-${item.nickname}`} src={item.thumbnail.small} />
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-evenly",
+          alignItems: "center",
+          marginX: 2,
+          marginY: 4,
+        }}
+      >
+        {isDesktop && (
+          <img
+            src={item.thumbnail.small}
+            alt={`chars-${item.nickname}`}
+            style={{ width: 250, height: 400 }}
+          />
+        )}
         <Box
           sx={{
-            flex: 1,
+            padding: 2,
+            backgroundColor: "rgba(150, 150, 150, 0.1)",
+            borderRadius: "10px",
+            boxShadow: "0 4px 30px rgba(0, 0, 0, 0.1)",
             display: "flex",
             flexDirection: "column",
             alignItems: "center",
           }}
         >
-          {/* wrapper input */}
-          <Box sx={{ marginBottom: 8 }}>
-            <Box style={{ display: "flex", columnGap: 20, marginBottom: 60 }}>
+          {/* wrapper for all input */}
+          <Box sx={{ marginBottom: 4 }}>
+            {/* wrapper input for name and age */}
+            <Box sx={{ display: "flex", columnGap: 4, marginBottom: 5 }}>
               <TextField
                 label="Name"
                 variant="standard"
@@ -72,7 +99,8 @@ function ModalEdit({ item, handleCloseModal }) {
                 sx={inputStyle}
               />
             </Box>
-            <Box style={{ display: "flex", columnGap: 20, marginBottom: 60 }}>
+            {/* wrapper input for type and category */}
+            <Box sx={{ display: "flex", columnGap: 4, marginBottom: 5 }}>
               <TextField
                 label="Type"
                 variant="standard"
@@ -86,20 +114,24 @@ function ModalEdit({ item, handleCloseModal }) {
                 sx={inputStyle}
               />
             </Box>
+            {/* quote */}
             <TextField
               label="Quote"
               variant="standard"
               value={item.quote}
               sx={inputStyle}
+              multiline
+              rows={2}
             />
           </Box>
-          {/* save button */}
+          {/* button */}
           <Button
             sx={{
+              backgroundColor: "#4169e1",
               color: "white",
-              fontSize: "15px",
-              background: "linear-gradient(315deg, #485461 0%, #28313b 74%)",
-              paddingX: "50px"
+              width: "200px",
+              borderRadius: "90px",
+              boxShadow: "0 10px 20px rgba(0, 0, 0, 0.2)",
             }}
           >
             Save
@@ -111,3 +143,30 @@ function ModalEdit({ item, handleCloseModal }) {
 }
 
 export default ModalEdit;
+
+
+{
+  /* <Box sx={{ display: "flex", columnGap: 1, marginX: 2 }}>
+        {item.ability.map((data) => (
+          <Box
+            sx={{
+              padding: "4px",
+              display: "flex",
+              alignItems: "center",
+              backgroundColor: "rgba(0, 0, 0, 0.65)",
+              borderRadius: "10px",
+              boxShadow: "0 4px 30px rgba(0, 0, 0, 0.1)",
+            }}
+          >
+            <Tooltip title={data.title}>
+              <img
+                src={data.icon}
+                alt={`skill-img`}
+                style={{ width: 130, height: 80, cursor: "pointer" }}
+              />
+            </Tooltip>
+            <Typography sx={{ color: "white" }}>{data.desc}</Typography>
+          </Box>
+        ))}
+      </Box> */
+}
